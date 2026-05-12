dc6fb39c8799660b3d691a47943359e4e52e817a
Enzo-Przy <enzo.przybylski@escola.pr.gov.br>
2026-05-12 07:55:12 -0300

    main js upd

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 const botoes = document.querySelectorAll(".botao")
 console.log(botoes)
+const textos = document.querySelectorAll(".aba-conteudo")
+console.log("textos")
+
+for(let i = 0; i<botoes.length; i++){
+    botoes[i].onclick = funcion () {
+        for(let j=onabort; j<botoes.length;j++){
+            botoes[j].classList.remove("ativo")
+            botoes[j].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativo")
+        botoes[i].classList.add("ativo")
+        }
+    }
+}
